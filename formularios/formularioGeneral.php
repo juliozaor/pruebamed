30a68352ee7be7481b20eb867062e759b60a238a
Miguel Velandia <miguel.velandia@manar.com.co>
2023-11-18 10:19:04 -0500

    selector con busqueda

--- formularios/formularioGeneral.php
+++ formularios/formularioGeneral.php
@@ -1,16 +1,18 @@
 <form>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  
+<div class="mb-3">
+    <label for="tipoconsulta" class="form-label">Tipo de consulta</label>
+    <select class="form-control choices-single">
+      <option value="CONSULTA DE CONTROL O SEGUIMIENTO POR MEDICINA GENERAL">CONSULTA DE CONTROL O SEGUIMIENTO POR
+        MEDICINA GENERAL</option>
+      <option value="CONSULTA PRIMERA VEZ POR MEDICINA GENERAL">CONSULTA PRIMERA VEZ POR MEDICINA GENERAL</option>
+    </select>
   </div>
+
   <div class="mb-3">
     <label for="exampleInputPassword1" class="form-label">Password</label>
     <input type="password" class="form-control" id="exampleInputPassword1">
   </div>
-  <div class="mb-3 form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+
+
 </form>
---
+++ index_tmp.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="css/style.css">
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/choices.js@9.0.1/public/assets/styles/choices.min.css"/>
+    
+</head>
+
+<body>
+  <div class="m-2">
+    <h1>Historia clinica morbilidad</h1>
+    <p>Para iniciar la grabación haga cick en 'Comenzar' y luego diga el nombre del campo que desea llenar. <br>
+      Si el campo tiene texto entre parentesis, ese es el que se debe mencionar. <br>
+      los campos se iran lleando despues que mencione el nobre</p>
+  </div>
+
+  <div class="m-2">
+    <button class="btn btn-success" id="btnStart">COMENZAR</button>
+    <button class="btn btn-danger" id="btnStop">DETENER</button>
+    <p><span class="grabando" id="grabando"> <strong>Grabando ....</strong> <br>
+
+        <small> para terminar, diga la palabra 'finalizar' o haga clic en el boton detener</small></span></p>
+
+  </div>
+  <div class="m-4">
+    <?php include 'formularios/formularioGeneral.php'; ?>
+  </div>
+  </div>
+  <script src="scripts/audio.js"></script>
+  <script src="scripts/procesarAudio.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+  </script>
+  <script src="https://cdn.jsdelivr.net/npm/choices.js@9.0.1/public/assets/scripts/choices.min.js"></script>
+
+</body>
+
+</html>
+
+<script>
+  $(function () {
+    $('select').selectpicker();
+    console.log('hola')
+  });
+</script>
